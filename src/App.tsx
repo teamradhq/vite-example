c5366d83400ecbb0f0f7b209628159e7a4f36cd2
Paul Beynon <paul@beynon.app>
2022-03-02 17:11:18 +1100

    INIT Vite react

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const updateCount = (increment: number) => () => {
+    setCount(count + increment);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={updateCount(1)}>
+            +
+          </button>
+          <p>count is: {count}</p>
+          <button type="button" onClick={updateCount(-1)}>
+            -
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
