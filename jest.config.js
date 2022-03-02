2be40209ecbce257d2863cec8790f0fcd74dc4cc
Paul Beynon <paul@beynon.app>
2022-03-03 10:39:25 +1100

    Configure jest

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  clearMocks: true,
+  collectCoverage: false,
+  coverageDirectory: 'coverage',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  coverageReporters: [
+    'text',
+    'html',
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/public',
+  ],
+  testMatch: [
+    '**/src/**/*.(test|spec).ts?(x)',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**'
+  ],
+};
