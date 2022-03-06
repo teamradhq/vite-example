ae50cd11503498470df11c674bc08a2ef1b78c54
Paul Beynon <paul@beynon.app>
2022-03-06 21:20:42 +1100

    Test store.tagSlice.process.tagKey

---
+++ src/store/tagsSlice/process/tagKey.test.ts
@@ -0,0 +1,21 @@
+import { tagKey } from '@src/store/tagsSlice/process/tagKey';
+
+import { uuid } from '@src/services/uuid';
+jest.mock('@src/services/uuid');
+const mockUuid = jest.mocked(uuid);
+
+describe('store.tag.process.tagKey', () => {
+  beforeAll(() => {
+    mockUuid.mockReturnValue('key');
+  });
+
+  it.each([[], []])('should generate a random key', () => {
+    expect.assertions(3);
+
+    const result = tagKey();
+
+    expect(uuid).toHaveBeenCalledTimes(1);
+    expect(uuid).toHaveBeenCalledWith({ prefix: 'tag' });
+    expect(result).toBe('key');
+  });
+});
