40399c617269876351351bf85a9bdb540c002ade
Paul Beynon <paul@beynon.app>
2022-03-09 16:43:09 +1100

    Set OLD_ENV in jest.setup

--- config/jest.setup.ts
+++ config/jest.setup.ts
@@ -9,7 +9,13 @@ if (typeof global.TextEncoder === 'undefined') {
 import { JSDOM } from 'jsdom';
 const dom = new JSDOM();
 
+declare global {
+  let OLD_ENV: NodeJS.ProcessEnv;
+}
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 (global as any).document = dom.window.document;
 (global as any).window = dom.window;
+(global as any).OLD_ENV = { ...process.env };
 /* eslint-enable @typescript-eslint/no-explicit-any */
+
