e4868c8bde41ac5cf218f20bcf40a26f60012436
Paul Beynon <paul@beynon.app>
2022-03-09 16:11:40 +1100

    Update environment annotations

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,17 @@
 /// <reference types="vite/client" />
-
 interface ImportMetaEnv {
   readonly APP_TITLE: string
+  readonly APP_DESCRIPTION: string
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
+}
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production';
+    APP_TITLE: string;
+    APP_DESCRIPTION: string;
+  }
 }
